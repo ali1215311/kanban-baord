a1ddd2735e6afa867baa2a019851145b2a82337c
Mohammad Ali <mohammad.ali1215311@gmail.com>
2025-12-23 17:27:35 +0600

    Initialize base project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import Board from "./board/Board";
+import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 
 const App = () => {
@@ -5,6 +7,10 @@ const App = () => {
     <>
       <div className="min-h-screen flex flex-col lg:flex-row">
         <Sidebar />
+        <main className="flex-1 flex flex-col min-h-0">
+          <Header />
+          <Board />
+        </main>
       </div>
     </>
   );
